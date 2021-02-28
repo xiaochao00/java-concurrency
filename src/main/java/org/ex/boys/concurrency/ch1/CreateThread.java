00659ee5f9149303350f17abf94784615cfc43f6
‘xiaochao00 <1363180272@qq.com>
2021-02-28 12:35:35 +0800

    添加创建线程的多种方式

--- src/main/java/org/ex/boys/concurrency/ch1/CreateThread.java
+++ src/main/java/org/ex/boys/concurrency/ch1/CreateThread.java
@@ -5,26 +5,43 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.FutureTask;
 
 public class CreateThread {
-    public static class CallerTask implements Callable<String>{
-
+    public static class CallerTask implements Callable<String> {
         public String call() throws Exception {
-            return "hello"; 
+            return "hello";
         }
-        
     }
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Runuale
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        };
+        Thread runableThread = new Thread(runnable);
+        runableThread.setName("runable-thread");
+        runableThread.start();
+        runableThread.join();
+        // Thread
+        Thread threadDemo = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(this.getName());
+            }
+        };
+        threadDemo.setName("thread-demo");
+        threadDemo.start();
+        threadDemo.join();
+
         FutureTask<String> futureTaskask = new FutureTask<String>(new CallerTask());
         new Thread(futureTaskask).start();
         try {
             String result = futureTaskask.get();
             System.out.println(result);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch (ExecutionException e) {
+        } catch (InterruptedException | ExecutionException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-
 }
